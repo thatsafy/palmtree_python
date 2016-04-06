4fe6db2ffad70a6c786c731aa64dc78ec57edde2
jukump <jukump@localhost>
2016-04-06 13:07:16 +0300

    lämpötilan parempi laskeminen

--- temperature.py
+++ temperature.py
@@ -17,27 +17,32 @@ def calcTemp(tr){
 
 			
 	value = value.split("x")		
-	
+
 	return value
 }
 
 while True:
-	
+
 	# x1U = port x1 U (voltage)
 	x1u = adc.read()/4095 * 3.3
-	
+
 	# front resistor R1 1780 ohm
 	r1 = 1790
-	
+
 	# MicroPython board output voltage
 	mpu = 3.3
 
 	# Temperature resistor KTY81/210 resistance
 	tr = x1u * r1 / (mpu - x1u)
-	
-	print(tr)
 
-	# Get temperature in celsius	
-	print (calcTemp(tr))
+	value = calcTemp(tr)
+
+	step = value[3] / (value[2] - value[0])
+
+	steps = tr - value[0]
+
+	temperature = value[1] + steps * step
+
+	print(temperature)
 
 	pyb.delay(500)
