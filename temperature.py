a41ba74d1b2f29800147c1d5f6eab4c8bc1b7120
Mafields <Mafields86@gmail.com>
2016-04-06 10:58:57 +0300

    update

--- temperature.py
+++ temperature.py
@@ -5,7 +5,18 @@ adc = ADC(Pin('X1'))
 
 while True:
 	
-	temp = adc.read() * 3.3 / 4095
-	print(temp)
+	# x1U = port x1 U (voltage)
+	x1u = adc.read()/4095 * 3.3
+	
+	# front resistor R1 1780 ohm
+	r1 = 1780
+	
+	# MicroPython board output voltage
+	mpu = 3.3
+
+	# Temperature resistor KTY81/210 resistance
+	tr = x1u * r1 / (mpu - x1u)
+	
+	print(tr)
 	
 	pyb.delay(500)
