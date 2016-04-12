f81db49647e7170cefe6746323ba998d819e0bca
Antti <anttialasalmi@gmail.com>
2016-04-12 07:10:12 +0000

    current main.py (message.py)

--- message.py
+++ message.py
@@ -1,21 +1,28 @@
-from pyb import UART, delay, Pin, ADC
+from pyb import UART, delay, Pin, ADC, I2C
 
 uart = UART(6, 115200)
 
+# Temperature
 adc = ADC(Pin('X1'))
 
 tempDict = {1630:'1630x1754x0x10',1772:'1772x1922x10x10',1922:'1922x2000x20x5'
         ,2000:'2000x2080x25x5',2080:'2080x2417x30x10'}
+
+# i2c = I2C(1)
+
 
 def message():
     m = ""
     m += measureTemp()
     m += ":"
     m += measureLight()
+    """
     m += ":"
     m += motorAngle()
     m += ":"
     m += numpad()
+    """
+    m += "\n"
     return m
 
 def measureTemp():
@@ -24,7 +31,7 @@ def measureTemp():
     # x1U = port x1 U (voltage)
     x1u = adc.read()/4095 * 3.3
 
-    # front resistor R1 1780 ohm
+    # front resistor R1 1790 ohm
     r1 = 1790
 
     # MicroPython board output voltage
@@ -52,8 +59,22 @@ def measureTemp():
     return str(temperature)
 
 def measureLight():
-    return str(450)
+    """
+    i2c.init(I2C.MASTER, baudrate=115200)
+    i2c.init(I2C.SLAVE, addr=0x43)
 
+    i2c.send(0x43)
+    data1 = i2c.recv(7)
+
+    i2c.send(0x83)
+    data2 = i2c.recv(7)
+
+    i2c.deinit()
+
+    tuloste = str(data1) + ":" + str(data2)
+    return tuloste
+    """
+    return str(2)
 def motorAngle():
     return str(360)
 
@@ -62,9 +83,10 @@ def numpad():
 
 def send():
     global uart
-    uart.write(bytes(message().encode('ascii')))
+    m = message()
+    uart.write(bytes(m.encode('ascii')))
 
-    print("send:",message())
+    print("send:", m)
 
 while True:
     send()  
