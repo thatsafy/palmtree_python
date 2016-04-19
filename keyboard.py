b89867e6c33f12a5a10ede7705d5cc94d30aa94d
Antti <anttialasalmi@gmail.com>
2016-04-19 09:04:23 +0300

    keyboard.py changes

--- keyboard.py
+++ keyboard.py
@@ -1,85 +1,51 @@
-#!/usr/bin/python3
-#
-# keypad16.py
-#
-# Jan.25/2015: v1.00 released
-#
-# 4x4 matrix keypad via MCP23017 I2C I/O expander  Python library
-# for the Raspberry Pi. Please see URL below for the introductory article.
-#
-# http://www.mikronauts.com/raspberry-pi/raspberry-pi-4x4-keypad-i2c-MCP23017-howto/
-#
-# Copyright 2015 William Henning
-# http://Mikronauts.com
-#
-# If the smbus module is re-entrant (ie allows multiple Python clients, keypad16
-# supports multiple keypad modules simultaneously, requires one port of an
-# MCP23017 I2C I/O port expander per keypad
-#
-# tested with generic 4x4 matrix keypad, right side up and up side down
-#
-# should also work unchanged with 4x3 keypad
-#
 import time
 from pyb import I2C
 
 class keypad_module:
 
-  i2c
+  i2c        = ""
   I2CADDR    = 0x20   	# valid range is 0x20 - 0x27
-  UPSIDEDOWN = 1      	# direction keypad is facing in
-  PORT       = 0      	# 0 for GPIOA, 1 for GPIOB
-
-  IODIRA = 0x00		# I/O direction register base address
-  PULUPA = 0x0C		# PullUp enable register base address
-  GPIOA  = 0x12		# GPIO pin register base address
-  OLATA  = 0x14		# Output Latch register base address
   
+  IODIR = 0x00		# I/O direction register base address
+  PULUP = 0x0C		# PullUp enable register base address
+  GPIO  = 0x12		# GPIO pin register base address
+    
   # Keypad Column output values
-  KEYCOL = [0b11110111,0b11111011,0b11111101,0b11111110]
+  COLS = [0b11101111, 0b10111111, 0b11111011]
+  ROWS = [0b11011111, 0b11110111, 0b11111101, 0b11111110]
+  MASKS = [0b00100000, 0b00001000, 0b00000010, 0b00000001]
 
   # Keypad Keycode matrix
-  KEYCODE  = [['1','4','7','*'], # KEYCOL0
-              ['2','5','8','0'], # KEYCOL1
-              ['3','6','9','#'], # KEYCOL2
-              ['A','B','C','D']] # KEYCOL3
 
-  # Decide the row
-  DECODE = [0,0,0,0, 0,0,0,0, 0,0,0,1, 0,2,3,0]
+  keys = {0x30: '1', 0x60: '2', 0x24: '3', 0x18: '4', 0x48: '5', 0x0C: '6', 0x012: '7', 0x42: '8', 0x06: '9', 0x11: '*', 0x41: 0, 0x05: '#'}
 
   # get a keystroke from the keypad
   def getch(self):
-    while 1:
-      for col in range(0,4):
-        time.sleep(0.01)
-        self.i2c.mem_write(self.I2CADDR, self.OLATA+self.port, self.KEYCOL[col]) # write 0 to lowest four bits
-        key = self.i2c.mem_read(1, self.I2CADDR, self.GPIOA+self.port) >> 4
-        if (key) != 0b1111:
-          row = self.DECODE[key]
-          while (self.i2c.mem_read(self.I2CADDR, self.GPIOA+self.port) >> 4) != 15:
-            time.sleep(0.01)
-          if self.UPSIDEDOWN == 0:
-            return self.KEYCODE[col][row] # keypad right side up
-          else:
-            return self.KEYCODE[3-row][3-col] # keypad upside down
+    for col in range(0,3)
+      print("col: %" % col)
+      self.i2c.mem_write(COLS[col], I2CADDR, IODIR)
+      self.i2c.mem_write(0xff, I2CADDR, PULUP)
+      time.sleep(0.01)
+      key = i2c.mem_read(1, I2CADDR, GPIO)
+      print("key: %" % key)
+      for m in MASKS:
+        if key & m != 0x00:
+          return (keys[key & m])
+      return "" 
 
   # initialize the keypad class
-  def __init__(i2c,addr,ioport,upside):
+  def __init__(self,i2c,addr):
     self.i2c = i2c
     self.I2CADDR = addr
-    self.UPSIDEDOWN = upside
-    self.port = ioport
-    self.i2c.mem_write(0xF0,self.I2CADDR,self.IODIRA+self.port) # upper 4 bits are inputs
-    self.i2c.mem_write(0xF0,self.I2CADDR,self.PULUPA+self.port) # enable upper 4 bits pullups
-
-i2cLCD = I2C(2, I2C.MASTER, baudrate=20000)
+    
+LCD = I2C(2, I2C.MASTER, baudrate=20000)
 
 # test code
 def main(): 
-  keypad = keypad_module(i2cLCD,0x20,0,0)  
+  keypad = keypad_module(LCD,0x20)  
   while 1:
     ch = keypad.getch()
-    print ch
+    print(ch)
 
     if ch == 'D':
       exit()
