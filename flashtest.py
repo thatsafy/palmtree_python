cd0ba66969b1d0d8256ffa1f2a81a1631f99136e
Mafields <Mafields86@gmail.com>
2016-04-21 12:44:06 +0300

    prints

--- flashtest.py
+++ flashtest.py
@@ -9,17 +9,23 @@ lcd_screen = char_lcd.HD44780(i2cLCD)
 def lcdWrite(row, stri):
     lcd_screen.set_line(row)
     lcd_screen.set_string(stri)
-
+    
 adc = ADC(Pin('X7'))
 
 # x1u = adc.read()
 lcdWrite(0, "makkara!!!")
 
-lights = []
-
-for i in range(0, 51):
-    lights[i] = 0
-
+lights = [0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,0,0]
+print(lights)
 while True:
     x1u = adc.read()
     if 0 in lights:
@@ -31,7 +37,8 @@ while True:
             lights[i] = lights[i+1]
         lights[50] = x1u
         sum = 0
-        for i in lights:
-            sum += i
+        for i in range(0,51):
+            sum += lights[i]
         x1u = sum/50
         lcdWrite(1, str(x1u))
+    print(lights)
