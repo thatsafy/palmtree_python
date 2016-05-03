e970b38ef0e92e86571c40bb6f7fcf24c2c4c529
Pahviboxi <aki.suhonen@hotmail.com>
2016-05-03 14:46:14 +0300

    pls work

--- motor.py
+++ motor.py
@@ -24,37 +24,68 @@ motorTuple = [(1,0,1,0),(1,0,0,0),(1,0,0,1),(0,0,0,1),(0,1,0,1),(0,1,0,0),(0,1,1
 # Delay between steps (min. 5ms)
 # stepDelay smaller = faster
 def rotatemotor(angle, stepDelay = 30):
-    # angle less than 4 will not move motor
-    if angle < 4:
-        angle = 4
-    # Add 1 to angle
-    angle = angle + 1
-    # Angle / step distance / step
-    angle = round(angle/0.9/len(motorTuple))
+
+	steps = 0
+	while angle >= 9:
+		steps += 10
+		angle -= 9
+	
+	if angle == 8:
+		steps += 9
+		angle -= 8
+		
+	elif angle == 7:
+		steps += 8
+		angle -= 7
+		
+	elif angle == 6:
+		steps += 7
+		angle -= 6
+
+	elif angle == 5:
+		steps += 6
+		angle -= 5
+		
+	elif angle == 4:
+		steps += 4
+		angle -= 4
+
+	elif angle == 3:
+		steps += 3
+		angle -= 3
+	
+	elif angle == 2:
+		steps += 2
+		angle -= 2
+		
+	elif angle == 1:
+		steps += 1
+		angle -= 1
+		
     stepDelay = stepDelay
     # enable stepper motor jumpers in L298
     Y8.high()
     Y3.high()
-    for i in range(0,angle):
-        for x in motorTuple:
-            print(x)
-            if x[0]:
-                Y4.high()
-            else:
-                Y4.low()
-            if x[1]:
-                Y5.high()
-            else:
-                Y5.low()
-            if x[2]:
-                Y6.high()
-            else:
-                Y6.low()
-            if x[3]:
-                Y7.high()
-            else:
-                Y7.low()
-            pyb.delay(stepDelay)
+    for i in range(0,(steps+1)):
+		x = i%8
+		print(x)
+		if x[0]:
+			Y4.high()
+		else:
+			Y4.low()
+		if x[1]:
+			Y5.high()
+		else:
+			Y5.low()
+		if x[2]:
+			Y6.high()
+		else:
+			Y6.low()
+		if x[3]:
+			Y7.high()
+		else:
+			Y7.low()
+		pyb.delay(stepDelay)
     # disable stepper motor jumpers in L298
     Y8.low()
     Y3.low()
